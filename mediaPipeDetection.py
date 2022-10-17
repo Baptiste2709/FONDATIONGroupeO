cfc010814594c11575e1aa71b649b3428b36e36e
Baptiste Michaud <baptiste.michaud@edu.ece.fr>
2022-10-17 12:37:26 +0200

    ajout de mediaPipeDetection.py, qui reconnait uniquement une chaussure pour l'instant

---
+++ mediaPipeDetection.py
@@ -0,0 +1,46 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+# setup mp
+mp_objectron = mp.solutions.objectron
+mp_drawing = mp.solutions.drawing_utils
+
+with mp_objectron.Objectron(static_image_mode=False,
+                            max_num_objects=2,
+                            min_detection_confidence=0.5,
+                            min_tracking_confidence=0.8,
+                            model_name='Shoe') as objectron:
+    while cap.isOpened():
+        start = time.time()
+
+        _, frame = cap.read()
+
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        frame.flags.writeable = False
+        results = objectron.process(frame)
+
+        frame.flags.writeable = True
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+        if results.detected_objects:
+            for detected_object in results.detected_objects:
+                mp_drawing.draw_landmarks(frame, detected_object.landmarks_2d, mp_objectron.BOX_CONNECTIONS)
+                mp_drawing.draw_axis(frame, detected_object.rotation, detected_object.translation)
+
+        end = time.time()
+        totalTime = end - start
+
+        fps = 1 / totalTime
+
+        cv2.putText(frame, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 2)
+
+        cv2.imshow('MediaPipe', frame)
+
+        if cv2.waitKey(1) == ord('a'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
--- objectDetector.py
+++ objectDetector.py
@@ -3,6 +3,9 @@ import numpy as np
 import time
 import os
 
+
+
+
 class Detector:
     def __init__(self, videoPath, configPath, modelPath, classesPath):
         self.videoPath = videoPath
@@ -24,14 +27,75 @@ class Detector:
             self.classesList = f.read().splitlines()
 
         self.classesList.insert(0, "__Background__")
+
+        self.colorList = np.random.uniform(0, 255, (len(self.classesList), 3))
         print(self.classesList)
 
+    def onVideo(self):
+        cap = cv2.VideoCapture(self.videoPath)
 
-cap = cv2.VideoCapture(0)
+        if(cap.isOpened() == False):
+            print("Error opening File...")
+            return
+
+        (success, image) = cap.read()
+
+        while success:
+            classLabelIDs, confidences, corners = self.net.detect(image, 0.4)
+
+            corners = list(corners)
+            confidences = list(np.array(confidences).reshape(1, -1)[0])
+            confidences = list(map(float, confidences))
+
+            cornerIDs = cv2.dnn.NMSBoxes(corners, confidences, score_threshold = 0.5, nms_threshold = 0.2)
+
+            if len(cornerIDs) != 0:
+                for i in range(0, len(cornerIDs)):
+                    corner = corners[np.squeeze(cornerIDs[i])]
+                    classConfidence = confidences[np.squeeze(cornerIDs[i])]
+                    classLabelID = np.squeeze(classLabelIDs[np.squeeze(cornerIDs[i])])
+                    classLabel = self.classesList[classLabelID]
+                    classColor = [int(c) for c in self.colorList[classLabelID]]
+
+                    displayText = "{}:{:.2f}".format(classLabel, classConfidence)
+
+                    x, y, w, h = corner
+
+                    cv2.rectangle(image, (x,y), (x+w, y+h), color = (255, 255, 255), thickness = 1)
+                    cv2.putText(image, displayText, (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 1, classColor, 2)
+
+                    ####################
+                    lineWidth = min(int(w * 0.3), int(h * 0.3))
+                    lineThickness = 4
+
+                    cv2.line(image, (x,y), (x + lineWidth, y), classColor, lineThickness)
+                    cv2.line(image, (x,y), (x, y + lineWidth), classColor, lineThickness)
+
+                    cv2.line(image, (x + w,y), (x + w - lineWidth, y), classColor, lineThickness)
+                    cv2.line(image, (x + w,y), (x + w, y + lineWidth), classColor, lineThickness)
+
+                    cv2.line(image, (x,y + h), (x + lineWidth, y + h), classColor, lineThickness)
+                    cv2.line(image, (x,y + h), (x,y + h - lineWidth), classColor, lineThickness)
+
+                    cv2.line(image, (x + w,y + h), (x + w - lineWidth, y + h), classColor, lineThickness)
+                    cv2.line(image, (x + w,y + h), (x + w,y + h - lineWidth), classColor, lineThickness)
+                    #####################
+            cv2.imshow("Result", image)
+
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('a'):
+                break
+
+            (success, image) = cap.read()
+        cv2.destroyAllWindows()
+
+
+
 videoPath = "videos/trafic_road.mp4"
 configPath = os.path.join("model_data", "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
 modelPath = os.path.join("model_data", "frozen_inference_graph.pb")
-classesPath = os.path.join("model_data", "coco_names")
+classesPath = os.path.join("model_data", "coco.names")
 
-Detector (videoPath, configPath, modelPath, classesPath)
+detector = Detector(videoPath, configPath, modelPath, classesPath)
+detector.onVideo()
 
